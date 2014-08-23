275dcd345c71fa37976e3b1f52bb7aa5cc42b940
Shuhei Toyonaga <toyonaga@akamaru-holdings.co.jp>
2014-08-23 11:39:13 +0900

    question apiのベース部分

--- src/python/api/vim_man.py
+++ src/python/api/vim_man.py
@@ -42,48 +42,49 @@ def test():
 # /questions
 @app.route('/questions', methods=['GET'])
 def index_questions():
-    cur = g.db.execute('select id, content, created_by, updated_by, created_at, updated_at from questions')
-    questions = [dict(id=row[0], content=row[1], created_by=row[2], updated_by=row[3], created_at=row[4], updated_at=row[5]) for row in cur.fetchall()]
-    app.logger.debug(questions)
-    return jsonify(result=questions)
+    code = 200
+    cur = g.db.execute('select id, content,state, created_by, updated_by, created_at, updated_at from questions')
+    questions = [dict(id=row[0], content=row[1], state=row[2], created_by=row[3], updated_by=row[4], created_at=row[5], updated_at=row[6]) for row in cur.fetchall()]
+    # app.logger.debug(questions)
+    return jsonify(status_code=code, result=questions)
 
 @app.route('/questions', methods=['POST'])
 def add_question():
+    code = 201
     #g.db.execute('insert into questions (content, created_by, updated_by, created_at, updated_at) values (?,?,?,?,?)',[])
     g.db.execute('insert into questions (content, state, created_by, updated_by, created_at, updated_at) values (?,?,?,?,?,?)',
             [request.form['content'], request.form['state'], request.form['created_by'], request.form['updated_by'], request.form['created_at'], request.form['updated_at']])
     g.db.commit()
-    #app.logger.debug(request.data)
-    app.logger.debug(request.form['content'])
-    app.logger.debug(request.form['state'])
-    app.logger.debug(request.form['created_by'])
-    app.logger.debug(request.form['updated_by'])
-    app.logger.debug(request.form['state'])
-    app.logger.debug(request.form['created_at'])
-    app.logger.debug(request.form['updated_at'])
-    return jsonify({0:3})
+
+    return jsonify(status_code=code)
 
 @app.route('/questions/<question_id>', methods=['GET'])
 def show_question(question_id):
-    cur = g.db.execute('select id, content, created_by, updated_by, created_at, updated_at from questions where id = 1')
+    code = 200
+    cur = g.db.execute('select id, content, created_by, updated_by, created_at, updated_at from questions where id = ?',
+            [question_id])
     question = [dict(id=row[0], content=row[1], created_by=row[2], updated_by=row[3], created_at=row[4], updated_at=row[5]) for row in cur.fetchall()]
-    #response.result = jsonify(question)
-    #response.status_code = 200
-    return jsonify(result=question)
+
+    return jsonify(status_code=code, result=question)
 
 @app.route('/questions/<question_id>', methods=['PUT'])
 def edit_question(question_id):
+    code = 201
     #app.logger.debug(request.form)
-    cur = g.db.execute('update questions set content="update_con", state=1 where id = 1')
+    cur = g.db.execute('update questions set content=?, state=?, created_by=?, updated_by=? where id = ?',
+            [request.form['content'], request.form['state'], request.form['created_by'], request.form['updated_at'], question_id])
     g.db.commit()
-    return jsonify({"request_method": "put"})
+
+    return jsonify(status_code=code)
 
 @app.route('/questions/<question_id>', methods=['DELETE'])
 def delete_question(question_id):
+    code = 204
     cur = g.db.execute('delete from questions where id = ?',
     [question_id])
     g.db.commit()
-    return jsonify({0:"deleteです"})
+
+    return jsonify(status_code=code)
 
 
 # /answers
