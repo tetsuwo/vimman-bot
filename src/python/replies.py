e6e1b558de7c66b93fb21021ee86b7b7d04c895b
masahero <masahero@masahero-PC.(none)>
2014-09-11 00:35:10 +0900

    リプライ

--- src/python/replies.py
+++ src/python/replies.py
@@ -8,10 +8,14 @@ customer_secret = secret.dict['customer_secret']
 access_token_key    = secret.dict['access_token_key']
 access_token_secret = secret.dict['access_token_secret']
 
-
 api = twitter.Api(customer_key,customer_secret,access_token_key,access_token_secret)
 
-retweet = api.GetReplies();
+replies = api.GetReplies();
 
-print retweet[0].user.screen_name
+text = '@%s retweet!!' % (replies[0].user.screen_name)
+to   = replies[0].id
+
+api.PostUpdate(
+    status=text,
+    in_reply_to_status_id=to)
 
