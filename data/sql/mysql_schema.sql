fd7520d4415537b544a9acdff0d716dcb15beec0
tetsuwo <tetsuwo.oishi@gmail.com>
2014-09-21 12:29:29 +0900

    SQL を data/sql 以下に移動

---
+++ data/sql/mysql_schema.sql
@@ -0,0 +1,75 @@
+DROP TABLE IF EXISTS operations;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS informations;
+DROP TABLE IF EXISTS tweets;
+DROP TABLE IF EXISTS responses;
+
+CREATE TABLE operations (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    salt VARCHAR(50),
+    state INTEGER,
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE questions (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    content TEXT,
+    state INTEGER,
+    created_by VARCHAR(50),
+    updated_by VARCHAR(50),
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE answers (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    question_id INTEGER,
+    content TEXT,
+    state INTEGER,
+    created_by VARCHAR(50),
+    updated_by VARCHAR(50),
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE informations (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    content TEXT,
+    state INTEGER,
+    created_by VARCHAR(50),
+    updated_by VARCHAR(50),
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tweets (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    type VARCHAR(10),
+    tweet_id INTEGER,
+    content TEXT,
+    created_by VARCHAR(50),
+    updated_by VARCHAR(50),
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE responses (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    type VARCHAR(10),
+    content TEXT,
+    state INTEGER,
+    created_by VARCHAR(50),
+    updated_by VARCHAR(50),
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    PRIMARY KEY (id)
+);
