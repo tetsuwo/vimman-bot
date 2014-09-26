8c010eb879f591e0768699ec59ae1c488a58f6a7
masahiiiro <nakano@akamaru-holdings.co.jp>
2014-09-27 03:20:01 +0900

    変更

--- src/python/secret.py
+++ src/python/secret.py
@@ -1,6 +1,6 @@
 dict = {
-    'customer_key' : 'JHGtxm2vDsYseAaHmY4eRshZx',
-    'customer_secret' : 'BIx42IfkG8xkbNy3bJ3VJM6Da5xRHP67k4o1HfleQSn9gxP1D1',
-    'access_token_key' : '2741926705-kz2mrIXj5YqZ47YdQcqoqaFs6qfQc6xjzOzQBT0',
-    'access_token_secret' : 'r9W9SWZUNjCrDPDeWqDGAL13gp7rFL41VmTJfR00inZrl'
+    'customer_key' : '',
+    'customer_secret' : '',
+    'access_token_key' : '',
+    'access_token_secret' : ''
    }
