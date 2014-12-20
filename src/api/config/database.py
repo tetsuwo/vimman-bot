7199f715ef7b04dad5612bc1bbb6caafd4ad2572
himejimasandesu <aganoyot@gmail.com>
2014-12-20 21:51:09 +0900

    変更が反映される設定を追加

---
+++ src/api/config/database.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+db_config = {
+    'host': 'localhost',
+    'db': 'vimmanbot',
+    'user': 'root',
+    'passwd': '',
+    'port': 3306,
+    'unix_socket': '/var/lib/mysql/mysql.sock'
+}
