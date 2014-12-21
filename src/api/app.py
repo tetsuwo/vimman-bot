37d317615b162d231e9042031740d2c0f4647b95
himejimasandesu <aganoyot@gmail.com>
2014-12-22 01:27:55 +0900

    お知らせ関連の修正

--- src/api/app.py
+++ src/api/app.py
@@ -537,30 +537,86 @@ def add_information():
     code = 201
     tdatetime = dt.now()
     tstr = tdatetime.strftime('%Y-%m-%d %H:%M:%S')
-    g.cursor.execute('insert into informations (content, state, created_by, updated_by, created_at, updated_at) values (%s, %s, %s, %s, %s, %s)',
-            [request.form['content'], request.form['state'], request.form['created_by'], request.form['updated_by'], tstr, tstr])
-    g.connection.commit()
+    req = request.form
+    # TODO SESSIONからloginユーザーをセットする
+    creator = "creator"
+    logging.debug(req['informations[content]'])
 
+    try:
+        information = Information(
+                                  id=None,
+                                  content=req['informations[content]'],
+                                  state=req['informations[state]'],
+                                  created_by=creator,
+                                  updated_by=creator,
+                                  created_at=tstr,
+                                  updated_at=tstr
+        )
+        db_session.add(information)
+        db_session.flush()
+        db_session.commit()
+    except:
+        # 登録失敗
+        pass
+    finally:
+        pass
+
+    # TODO redirect処理を追加
     return jsonify(status_code=code)
 
 @app.route('/informations', methods=['GET'])
 @crossdomain(origin='*')
 def index_informations():
+    """お知らせの一覧を取得します
+    """
     code = 200
-    g.cursor.execute('select id, content, state, created_by, updated_by, created_at, updated_at from informations')
-    informations = [dict(id=row[0], content=row[1], state=row[2], created_by=row[3], updated_by=row[4], created_at=row[5], updated_at=row[6]) for row in g.cursor.fetchall()]
-    # app.logger.debug(questions)
-    return jsonify(status_code=code, result=informations)
+
+    try:
+        informations = get_informations()
+        informations_dict = ListInformationMapper({'result': informations}).as_dict()
+    except:
+        pass
+
+    result = informations_dict['result']
+
+    return jsonify(status_code=code, result=result)
 
 @app.route('/informations/<information_id>', methods=['GET'])
 @crossdomain(origin='*')
 def show_information(information_id):
     code = 200
-    g.cursor.execute('select id, content, state, created_by, updated_by, created_at, updated_at from informations where id = %s',
-            [information_id])
-    information = [dict(id=row[0], content=row[1], state=row[2], created_by=row[3], updated_by=row[4], created_at=row[5], updated_at=row[6]) for row in g.cursor.fetchall()]
+    information_dict = {}
 
-    return jsonify(status_code=code, result=information)
+    try:
+        information = get_information(information_id)
+        information_dict = InformationMapper(information).as_dict()
+    except:
+        # 取得に失敗
+        pass
+
+    return jsonify(status_code=code, result=information_dict)
+
+def get_information(information_id):
+    information = []
+    res = Information.query.filter("id = :information_id").params(information_id=information_id).first()
+    information = Information(id=information_id,
+                              content=res.content,
+                              state=res.state,
+                              created_by=res.created_by,
+                              updated_by=res.updated_by,
+                              created_at=res.created_at,
+                              updated_at=res.updated_at
+    )
+
+    return information
+
+def get_informations():
+    informations = []
+    res = Information.query.all()
+    for row in res:
+        informations.append(row)
+
+    return informations
 
 @app.route('/informations/<information_id>', methods=['PUT'])
 @crossdomain(origin='*')
@@ -568,19 +624,39 @@ def edit_information(information_id):
     code = 201
     tdatetime = dt.now()
     tstr = tdatetime.strftime('%Y-%m-%d %H:%M:%S')
-    g.cursor.execute('update informations set content = %s, state = %s, updated_by = %s, updated_at = %s where id = %s',
-            ([request.form['content'], request.form['state'], request.form['updated_by'], tstr, information_id]))
-    g.connection.commit()
+    req = request.form
+    # TODO sessionから取得する
+    updater = "updater"
+    try:
+        row = db_session.query(Information).get(information_id)
+        row.content = req['informations[content]']
+        row.state = req['informations[state]']
+        row.updated_by = updater
+        row.updated_at = tstr
+        db_session.flush()
+    except:
+        pass
+    finally:
+        pass
+
 
     return jsonify(status_code=code)
 
 @app.route('/informations/<information_id>', methods=['DELETE'])
 @crossdomain(origin='*')
 def delete_information(information_id):
+    """お知らせを削除します
+    """
     code = 204
-    g.cursor.execute('delete from informations where id = %s',
-    [information_id])
-    g.connection.commit()
+
+    try:
+        row = Information.query.get(information_id)
+        db_session.delete(row)
+        db_session.flush()
+    except:
+        pass
+    finally:
+        pass
 
     return jsonify(status_code=code)
 
