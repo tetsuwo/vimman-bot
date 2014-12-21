6e75145459121803d76c2f0189346602061384ef
himejimasandesu <aganoyot@gmail.com>
2014-12-21 23:27:02 +0900

    modelを追加

--- src/api/app.py
+++ src/api/app.py
@@ -66,6 +66,23 @@ def teardown_request(exception):
 
 # モデルクラス TODO 外部に出す
 # operationsクラス
+class Operation(Base):
+    __tablename__ = 'operations'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    password = Column(String(50))
+    salt = Column(String(50))
+    state = Column(Integer)
+    created_at = Column(DateTime, default=dt.now)
+    updated_at = Column(DateTime, default=dt.now)
+
+    def __init__(self, id, username, salt, state, created_at, updated_at):
+        self.id = id
+        self.username = username
+        self.salt = salt
+        self.state = state
+        self.created_at = created_at
+        self.updated_at = updated_at
 
 # questionsテーブルのmodel
 class Question(Base):
@@ -88,12 +105,90 @@ class Question(Base):
         self.updated_at = updated_at
 
 # answersクラス
+class Answer(Base):
+    __tablename__ = 'answers'
+    id = Column(Integer, primary_key=True)
+    question_id = Column(Integer)
+    content = Column(String)
+    state = Column(Integer)
+    created_by = Column(String(50))
+    updated_by = Column(String(50))
+    created_at = Column(DateTime, default=dt.now)
+    updated_at = Column(DateTime, default=dt.now)
+
+    def __init__(self, id, question_id, content, state, created_by, updated_by, created_at, updated_at):
+        self.id = id
+        self.question_id = question_id
+        self.content = content
+        self.state = state
+        self.created_by = created_by
+        self.updated_by = updated_by
+        self.created_at = created_at
+        self.updated_at = updated_at
 
 # informationsクラス
+class Information(Base):
+    __tablename__ = 'informations'
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+    state = Column(Integer)
+    created_by = Column(String(50))
+    updated_by = Column(String(50))
+    created_at = Column(DateTime, default=dt.now)
+    updated_at = Column(DateTime, default=dt.now)
+
+    def __init__(self, id, content, state, created_by, updated_by, created_at, updated_at):
+        self.id = id
+        self.content = content
+        self.state = state
+        self.created_by = created_by
+        self.updated_by = updated_by
+        self.created_at = created_at
+        self.updated_at = updated_at
 
 # tweetsクラス
+class Tweet(Base):
+    __tablename__ = 'tweets' 
+    id = Column(Integer, primary_key=True)
+    type = Column(String(10))
+    tweet_id = Column(Integer)
+    content = Column(String)
+    created_by = Column(String(50))
+    updated_by = Column(String(50))
+    created_at = Column(DateTime, default=dt.now)
+    updated_at = Column(DateTime, default=dt.now)
+
+    def __init__(self, id, type, tweet_id, content, created_by, updated_by, created_at, updated_at):
+        self.id = id
+        self.type = type
+        self.tweet_id = tweet_id
+        self.content = content
+        self.created_by = created_by
+        self.updated_by = updated_by
+        self.created_at = created_at
+        self.updated_at = updated_at
 
 # responsesクラス
+class Response(Base):
+    __tablename__ = 'responses' 
+    id = Column(Integer, primary_key=True)
+    type = Column(String(10))
+    content = Column(String)
+    state = Column(Integer)
+    created_by = Column(String(50))
+    updated_by = Column(String(50))
+    created_at = Column(DateTime, default=dt.now)
+    updated_at = Column(DateTime, default=dt.now)
+
+    def __init__(self, id, type, content, state, created_by, updated_by, crated_at, updated_at):
+        self.id = id
+        self.type = type
+        self.content = content
+        self.state = state
+        self.created_by = created_by
+        self.updated_by = updated_by
+        self.created_at = created_at
+        self.updated_at = updated_at
 
 # マッパークラス TODO 外に出すこと
 ## Mapper For Question
