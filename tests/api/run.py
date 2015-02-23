cd8a2c2faa547ebfca12b5f3e19af8d33da267c0
Tetsuwo OISHI <tetsuwo.oishi@gmail.com>
2015-02-24 01:13:12 +0900

    Update run.py

--- tests/api/run.py
+++ tests/api/run.py
@@ -1,16 +1,36 @@
-import sys
-import os
-import unittest
+import sys, os, json, unittest
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../src/api/')
 import app
 
 class ApiTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
+        print 'completed setup'
 
-    def test_get(self):
+    def test_get1(self):
         print 'testing...'
         response = self.app.get('/questions/')
+        print response.status_code
+        #assert response.status_code == 200
+        #assert response.data == 'Hello, World!'
+        print response.data
+
+    def test_get2(self):
+        response = self.app.get('/questions')
+        print response.status_code
+        #assert response.status_code == 200
+        #assert response.data == 'Hello, World!'
+        print response.data
+
+    def test_get3(self):
+        response = self.app.get('/api/questions')
+        print response.status_code
+        #assert response.status_code == 200
+        #assert response.data == 'Hello, World!'
+        print response.data
+
+    def test_get4(self):
+        response = self.app.get('/questions?hoge')
         print response.status_code
         #assert response.status_code == 200
         #assert response.data == 'Hello, World!'
