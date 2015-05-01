06ea1d719a7765d7394a29e27763f23718f18abb
aganoyot <aganoyot@gmail.com>
2015-05-02 00:13:30 +0900

    tweetのtest追加

--- tests/api/api_tweets_test.py
+++ tests/api/api_tweets_test.py
@@ -9,11 +9,10 @@ class ApiTweetsTestCase(unittest.TestCase):
 
     def test_create(self):
         content_body = {
-            'type' : 'tester-1',
-            'tweet_id' : 'hogehoge',
-            'content' : '2',
-	    'state' : '2',
-            'post_url' : ''
+            'type' : 'response',
+            'tweet_id' : '1',
+            'content' : 'tweet content',
+            'post_url' : 'http://www.yahoo.co.jp/'
         }
         raw_response = self.app.post(
             '/tweets/',
@@ -24,61 +23,63 @@ class ApiTweetsTestCase(unittest.TestCase):
         response = json.loads(raw_response.data)
         assert response['result'] != ''
         assert response['result']['id'] != ''
-        assert response['result']['type'] == 2
-        assert response['result']['tweet_id'] == ''
-        assert response['result']['content'] == ''
-        assert response['result']['state'] == ''
-        assert response['result']['post_url'] == ''
+        assert response['result']['type'] == 'response'
+        assert response['result']['tweet_id'] == 1
+        assert response['result']['content'] == 'tweet content'
 
-    #def test_invalid_create(self):
-    #    content_body = {
-    #        'password' : 'hogehoge',
-    #        'state'    : '2'
-    #    }
-    #    raw_response = self.app.post(
-    #        '/operators/',
-    #        content_type='application/json',
-    #        data=json.dumps(content_body)
-    #    )
-    #    assert raw_response.status_code == 400
+    def test_invalid_create(self):
+        content_body = {
+            'type' : 'question',
+            'tweet_id' : '2',
+            'content' : 'tweet content invalid',
+        }
+        raw_response = self.app.post(
+            '/tweets/',
+            content_type='application/json',
+            data=json.dumps(content_body)
+        )
+        assert raw_response.status_code == 400
 
-    #def test_index(self):
-    #    raw_response = self.app.get(
-    #        '/operators/'
-    #    )
-    #    assert raw_response.status_code == 200
-    #    response = json.loads(raw_response.data)
-    #    assert response['result'] != ''
-    #    assert response['result'][0]['id'] is not None
-    #    assert response['result'][0]['state'] is not None
-    #    assert response['result'][0]['username'] is not None
+    def test_index(self):
+        raw_response = self.app.get(
+            '/tweets/'
+        )
+        assert raw_response.status_code == 200
+        response = json.loads(raw_response.data)
+        assert response['result'] != ''
+        assert response['result'][0]['id'] is not None
+        assert response['result'][0]['tweet_id'] is not None
+        assert response['result'][0]['content'] is not None
+        assert response['result'][0]['post_url'] is not None
 
-    #def test_read(self):
-    #    content_body = {
-    #        'username' : 'tester-2',
-    #        'password' : 'hogehoge',
-    #        'state'    : '1'
-    #    }
-    #    raw_response = self.app.post(
-    #        '/operators/',
-    #        content_type='application/json',
-    #        data=json.dumps(content_body)
-    #    )
-    #    created = json.loads(raw_response.data)
-    #    raw_response = self.app.get(
-    #        '/operators/%d' % created['result']['id']
-    #    )
-    #    assert raw_response.status_code == 200
-    #    response = json.loads(raw_response.data)
-    #    assert response['result']['id'] == created['result']['id']
-    #    assert response['result']['state'] == created['result']['state']
-    #    assert response['result']['username'] == created['result']['username']
+    def test_read(self):
+        content_body = {
+            'type' : 'response',
+            'tweet_id' : '3',
+            'content' : 'tweet content3',
+            'post_url' : 'http://www.yahoo.co.jp/'
+        }
+        raw_response = self.app.post(
+            '/tweets/',
+            content_type='application/json',
+            data=json.dumps(content_body)
+        )
+        created = json.loads(raw_response.data)
+        raw_response = self.app.get(
+            '/tweets/%d' % created['result']['id']
+        )
+        assert raw_response.status_code == 200
+        response = json.loads(raw_response.data)
+        assert response['result']['id'] == created['result']['id']
+        assert response['result']['tweet_id'] == created['result']['tweet_id']
+        assert response['result']['content'] == created['result']['content']
+        assert response['result']['post_url'] == created['result']['post_url']
 
-    #def test_unknown_read(self):
-    #    raw_response = self.app.get(
-    #        '/operators/%d' % 1000000
-    #    )
-    #    assert raw_response.status_code == 404
+    def test_unknown_read(self):
+        raw_response = self.app.get(
+            '/tweets/%d' % 1000000
+        )
+        assert raw_response.status_code == 404
 
 if __name__ == '__main__':
     unittest.main()
