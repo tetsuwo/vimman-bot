2eab8d748b8dfc8fce7ff85977657f61f477f2f1
tetsuwo <tetsuwo.oishi@gmail.com>
2015-05-03 16:55:49 +0900

    Add twitter name on settings

--- src/bot/config/settings.py
+++ src/bot/config/settings.py
@@ -10,5 +10,6 @@ Config = {
     'consumer_secret': os.environ.get('TWITTER_CONSUMER_SECRET'),
     'access_token': os.environ.get('TWITTER_ACCESS_TOKEN'),
     'access_token_secret': os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
-    'vimmanbot_api_url': os.environ.get('VIMMANBOT_API_URL'),
+    'api_url': os.environ.get('VIMMANBOT_API_URL'),
+    'twitter_name': 'vimmanbot',
 }
