0378d35ef9d3789305d83b7dd05ba5d23d7dbc3e
tetsuwo <tetsuwo.oishi@gmail.com>
2015-05-03 12:23:49 +0900

    Refactor replies.py

--- src/bot/replies.py
+++ src/bot/replies.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from requests_oauthlib import OAuth1Session
-import json
-import secret
+from config.settings import *
+import sys, json
 
-c_k = secret.dict['customer_key']
-c_s = secret.dict['customer_secret']
-a_k = secret.dict['access_token_key']
-a_s = secret.dict['access_token_secret']
+c_k = Config['consumer_key']
+c_s = Config['consumer_secret']
+a_k = Config['access_token']
+a_s = Config['access_token_secret']
 
-url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json"
+MENSION_API_ENDPOINT = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json'
+UPDATE_API_ENDPOINT = 'https://api.twitter.com/1.1/statuses/update.json'
 
 twitter = OAuth1Session(c_k, c_s, a_k, a_s)
+reply_count = 0
 
-params = {}
+raw_response = twitter.get(MENSION_API_ENDPOINT, params={})
+if raw_response.status_code == 200:
+    response = json.loads(raw_response.text)
+    for tweet in response:
+        res_tweet = '@%s wah gwaan' % (tweet['user']['screen_name'])
 
-req = twitter.get(url, params = params)
+        request_params = {
+            'status': res_tweet,
+            'in_reply_to_status_id': tweet['user']['id_str']
+        }
+        tweet_raw_response = twitter.post(UPDATE_API_ENDPOINT, params=request_params)
 
-if req.status_code == 200:
-    timeline = json.loads(req.text)
-
-    url = "https://api.twitter.com/1.1/statuses/update.json"
-    for tweet in timeline:
-        res_tweet = '@%s wah gwaan' % (tweet["user"]["screen_name"])
-        params = {"status":res_tweet, "in_reply_to_status_id":tweet['user']["id_str"]}
-
-        req = twitter.post(url, params = params)
-        
-        if req.status_code == 200:
-            print ("OK")
+        if tweet_raw_response.status_code == 200:
+            print 'OK'
         else:
-            print ("Error: %d" % req.status_code)
+            print 'Error:', tweet_raw_response.status_code
+
+        reply_count += 1
 else:
-    # エラーの場合
-    print ("Error: %d" % req.status_code)
+    print 'Error:', raw_response.status_code
+
+print 'REPLY COUNT:', reply_count
+
+
