4cb799ef3aed82c4cc37811236d43dca8574cac0
tetsuwo <tetsuwo.oishi@gmail.com>
2015-05-07 21:24:56 +0900

    Add test script

--- src/bot/test.py
+++ src/bot/test.py
@@ -6,3 +6,9 @@ from config.settings import *
 r = requests.get('http://www.vimmanbot.local/api/informations/')
 print r.status_code
 print r.text
+print r
+
+r = requests.get('http://www.vimmanbot.local/api/questions/')
+print r.status_code
+print r.text
+print r
